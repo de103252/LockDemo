0aa8dac693b0e4bfbc9f1181bb9d3a8cf05ff221
Uli Seelbach <ulrich.seelbach@de.ibm.com>
2026-02-11 20:54:16 +0100

    Add missing requires.

--- src/main/java/com/ibm/de103252/lockdemo/module-info.java
+++ src/main/java/com/ibm/de103252/lockdemo/module-info.java
@@ -1,5 +1,8 @@
 module LockDemo {
     requires java.desktop;
     requires java.sql;
+    requires java.sql.rowset;
     requires java.prefs;
+    requires org.fife.RSyntaxTextArea;
+    requires autocomplete;
 }
